a2bc8b7d1ea66eea4feea5820ea460aebd9a8717
Igor Zheltukhin <igor.zheltukhin@winfinity.live>
2025-03-30 21:23:04 +0400

    generics in bus manager

--- internals/bus/bus_manager.go
+++ internals/bus/bus_manager.go
@@ -5,18 +5,18 @@ import (
 	"sync"
 )
 
-type Manager struct {
-	Buses map[string]*Bus[[]byte]
+type Manager[T any] struct {
+	Buses map[string]*Bus[T]
 	mu    sync.RWMutex
 }
 
-func NewManager() *Manager {
-	return &Manager{
-		Buses: make(map[string]*Bus[[]byte]),
+func NewManager[T any]() *Manager[T] {
+	return &Manager[T]{
+		Buses: make(map[string]*Bus[T]),
 	}
 }
 
-func (m *Manager) GetBusSet() map[string]bool {
+func (m *Manager[T]) GetBusSet() map[string]bool {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
 	set := make(map[string]bool)
@@ -26,7 +26,7 @@ func (m *Manager) GetBusSet() map[string]bool {
 	return set
 }
 
-func (m *Manager) Subscribe(id string) (chan []byte, error) {
+func (m *Manager[T]) Subscribe(id string) (chan T, error) {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
 	if bus, ok := m.Buses[id]; !ok {
@@ -36,7 +36,7 @@ func (m *Manager) Subscribe(id string) (chan []byte, error) {
 	}
 }
 
-func (m *Manager) Unsubscribe(id string, ch chan []byte) error {
+func (m *Manager[T]) Unsubscribe(id string, ch chan T) error {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
 	if bus, ok := m.Buses[id]; !ok {
@@ -46,18 +46,18 @@ func (m *Manager) Unsubscribe(id string, ch chan []byte) error {
 	}
 }
 
-func (m *Manager) AddBus(id string) (*Bus[[]byte], error) {
+func (m *Manager[T]) AddBus(id string) (*Bus[T], error) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	if _, ok := m.Buses[id]; ok {
 		return nil, fmt.Errorf("bus %s already exists", id)
 	}
-	bus := NewBus[[]byte](1000)
+	bus := NewBus[T](1000)
 	m.Buses[id] = bus
 	return bus, nil
 }
 
-func (m *Manager) DeleteBus(id string) error {
+func (m *Manager[T]) DeleteBus(id string) error {
 	m.mu.Lock()
 	defer m.mu.Unlock()
 	if _, ok := m.Buses[id]; !ok {
